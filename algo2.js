4867b89c653e51b8924869163190ca20a6d4bf19
martabtab <57708251+martabtab@users.noreply.github.com>
2020-02-14 15:33:49 +0100

    modified
    modified

--- algo2.js
+++ algo2.js
@@ -1,4 +1,4 @@
-/* 
+/*
 The new guy in your team wrote this terrible code
 and now he's asking you why it doesn't work...
 
@@ -21,21 +21,24 @@ Result: [7, 85]
 
 Fix the following function to help your new coworker!
 
-Note: try changing only the necessary code, don't rewrite the entire thing. 
+Note: try changing only the necessary code, don't rewrite the entire thing.
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+let numbers = [12, 3, 24];
 
-    for (let i = 1; i = array.length - 1; i + 1) {
-        if (array(i) < min) {
-            min = array;
-        }
-        if (array[i] = max) {
-            max = array[i];
-        }
-        return array;
+let minMax = array => {
+  let min = array[0];
+  let max = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    } else if (array[i] > max) {
+      max = array[i];
     }
     array.push(min, max);
-}
+  }
+  return array;
+};
+
+console.log(minMax(numbers));
--- algo3.js
+++ algo3.js
@@ -1,10 +1,26 @@
 /*
 Given a number that represents the position of the minute hand on a clock (number of minutes from 0 - 60) write a program that returns the angle that is formed by the minute hand and 12.
 
-If the user enters a number that is above 60, your program should fail, and log a friendly message to the console, informing the user of their error. 
+If the user enters a number that is above 60, your program should fail, and log a friendly message to the console, informing the user of their error.
 (Hint: Create an HTML UI to capture input and display the result of running you algorithm)
 */
 
 function minuteAngle(minute) {
-
+  let number = document.getElementById("inputNumber").value;
+  if (number > 60) {
+    document.getElementById("message").innerHTML = "Please over 60!";
+  } else {
+    let angle = number * 6;
+    let difference = 0;
+    if (192 <= angle && angle <= 360) {
+      difference = 372 - angle;
+    } else if (12 <= angle && angle < 192) {
+      difference = angle - 12;
+    } else if (0 >= angle && angle < 12) {
+      difference = 12 - angle;
+    }
+    console.log(angle);
+    console.log(difference);
+    document.getElementById("message").innerHTML = difference + "°";
+  }
 }
